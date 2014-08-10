c3647770cfe45d64d95837cd02223d5cabc0460b
Ting <tinghf@gmail.com>
2014-08-10 13:36:46 -0700

    remove unnecessary function

--- load_data.R
+++ load_data.R
@@ -2,10 +2,6 @@
 # Load data 
 # data from: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
-datetime <- function(date, time) {
-    return (strptime(paste(date, time), "%d/%m/%Y %H:%M:%S"))
-}
-
 loaddata <- function() {
     filename <- "exdata-data-household_power_consumption/household_power_consumption.txt"
     df <- read.table(filename,
